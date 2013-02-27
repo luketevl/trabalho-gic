c29868693f2916b209b7a69d6eb9cb340245d36b
luketevl <luketevl@b8303ca9-a0fe-04db-0b07-ae62dc87b2d0>
2013-02-27 19:54:30 +0000

    - Setando helper(form) e library(database) no application/config/autoload - Configurando database no no application/config/database - Removendo index.php da URL - Acrescentando jquery - Acrescentando pasta resource para css/js/img etc.

---
+++ trunk/application/views/testes.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+	<meta charset="utf-8">
+	<title><?php echo ($Titulo) ?></title>
+</head>
+<body>
+
+<div id="container">
+	<h1><?php echo $Mensagem ?></h1>
+
+	<div id="body">
+		<?php 
+			echo form_open('testes/login');
+			echo form_fieldset('Campos');
+			echo form_input('User');
+			echo form_password('pass');
+			echo form_submit('valida','Validar');
+			Echo form_fieldset_close();
+			echo form_close();
+		?>
+	</div>
+
+	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+</div>
+
+</body>
+</html>
