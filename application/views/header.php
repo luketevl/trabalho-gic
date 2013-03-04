33e9a84dc958b81efb7182e8a69ce8c7122d39c7
luketevl <luketevl@b8303ca9-a0fe-04db-0b07-ae62dc87b2d0>
2013-03-04 20:44:36 +0000

    - Criando layout padrao

--- application/views/header.php
+++ application/views/header.php
@@ -5,18 +5,93 @@
 	<title>Layout Padrao</title>
 	<link rel="stylesheet" type="text/css" href="resources/css/style.css" /> 
 <style>
+*{
+	margin:0px;	
+}
+	header#cabecalho{
+		background-color: red;
+		width:100%;
+		height:170px;
+		border-bottom-width:1.3px;
+		border-bottom-style:solid;
+	}
 	
+	header{
+	    border-bottom: 1px;
+    	border-bottom-style: solid;
+	
+	}
+	section#conteudos{
+		width:100%;
+		height:300px;
+	}
+
+	section{
+		width:20%;
+		height:300px;
+	}
+	section#column-left{
+		background-color: yellow;
+		float:left;
+		border-right-width:1px;
+		border-right-style:solid;
+	}
+	section#column-right{
+		background-color: pink;
+		float:right;
+		border-left-width:1px;
+		border-left-style:solid;
+	}
+	
+	section#center{
+		background-color: green;
+		width:59.85%;
+		float:left;
+	}
+
+	footer{
+		background-color: blue;
+		width:100%;
+		height:200px;
+		border-top-width:1px;
+		border-top-style:solid;
+	}	
+	
+
 	
 </style>
 
 </head>
 <body>
+<header id="cabecalho">
+	<h1> Comeco</h1>
+<nav>
+	
+</nav>
+</header>
+<section id="conteudos">
+	<section id="column-left" >
+		<header>Social</header>
+	</section>
+	
+	<section id="center" >
+		<header>Materias</header>
+		<article>
+			<img src="resources/img/teste.jpg" width="200" alt="Imagem teste" />
+			<a href="#" title="link">Veja sobre a coisa do pedaço</a>
+		</article>
+	
+	</section>
+	
+	<section id="column-right" >
+		<header>Videos</header>
+	</section>
+</section>
 
 
-
-
-
-
+<footer>
+	<h3>Rodape</h3>
+</footer>
 </body>
 </html>
 
