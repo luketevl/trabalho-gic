e6f6acbc5b766abad4ff909e909576a8fa564ccb
luketevl <luketevl@b8303ca9-a0fe-04db-0b07-ae62dc87b2d0>
2013-04-17 07:11:55 +0000

    - Acrescentado layout flat-ui

--- trunk/application/controllers/intranet/perfil_cadastro.php
+++ trunk/application/controllers/intranet/perfil_cadastro.php
@@ -9,12 +9,14 @@ class Perfil_cadastro extends CI_Controller {
 	}
 	
 	public function save(){
-		$this->load->model('intranet/perfil_dados');
 		$dados = array(
 			'id_perf' 	     => null, 
 			'nome_perf'	 	 => ($this->input->post('nome')),
-			'funcoes_perf'	 => ($this->input->post('ck_criar') . $this->input->post('ck_aprovar') .$this->input->post('ck_editar').$this->input->post('ck_apagar'))
+			'funcoes_perf'	 => ($this->input->post('ck_criar') . $this->input->post('ck_aprovar') .$this->input->post('ck_alterar').$this->input->post('ck_apagar')),
+			'acesso_perf'	 => ($this->input->post('ck_criar') . $this->input->post('ck_aprovar') .$this->input->post('ck_alterar').$this->input->post('ck_apagar'))
 		);
+        print_r($dados);
+        die;
 		if($this->perfil_dados($dados)){
 			redirect('welcome');	
 		}
