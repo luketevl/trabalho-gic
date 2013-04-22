834be6d027dbc183940ca23c4d945a227d248656
luketevl <luketevl@b8303ca9-a0fe-04db-0b07-ae62dc87b2d0>
2013-04-22 19:39:59 +0000

    - Adicionado plugin de notificação.

---
+++ trunk/application/controllers/intranet/perfil_cadastro_list.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Perfil_Cadastro_List extends CI_Controller{
+	
+	function index(){
+		$this->load->view('intranet/perfil_cadastro_list');
+	}
+}
+
+/* End of file perfil_cadastro_list.php */
+/* Location: ./application/controllers/intranet/perfil_cadastro_list.php */
