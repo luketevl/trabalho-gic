8bf8f785f640c1a1f6f60e59b69b2d096097374d
luketevl <luketevl@b8303ca9-a0fe-04db-0b07-ae62dc87b2d0>
2013-04-24 21:14:13 +0000

    - Criando menu e adicionado pasta de icons

--- application/controllers/intranet/principal.php
+++ application/controllers/intranet/principal.php
@@ -3,10 +3,18 @@
 class Principal extends CI_Controller{
 	
 	function index(){
-		$menu = array('principal' 	=> base_url().'index.php/intranet/principal',
-					  'usuarios'	=> base_url().'index.php/intranet/usuarios_cadastro_list',
-					  'perfil'		=> base_url().'index.php/intranet/perfil_cadastro_list',
-					  'materias'	=> base_url().'index.php/intranet/materias_cadastro_list'
+		$this->lang->load('menu');
+		$menu = array('principal' 	=> array('url' => base_url().'index.php/intranet/principal' ,
+											 'name' => $this->lang->line('principal')),
+				
+					  'usuarios'	=> array('url' =>base_url().'index.php/intranet/usuarios_cadastro_list',
+					  						 'name' => $this->lang->line('usuarios')),
+				
+					  'perfil'		=> array('url' =>base_url().'index.php/intranet/perfil_cadastro_list',
+					  						 'name' =>$this->lang->line('perfil')),
+					  		
+					  'materias'	=> array('url' =>base_url().'index.php/intranet/materias_cadastro_list',
+					  						 'name' => $this->lang->line('materias'))
 				);
 		$this->load->view('intranet/principal',$menu);
 	}
