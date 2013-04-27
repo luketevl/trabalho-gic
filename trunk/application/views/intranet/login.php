ecf924cf794bf36fd5fe67d94fba26e4ec5a3d5b
luketevl <luketevl@b8303ca9-a0fe-04db-0b07-ae62dc87b2d0>
2013-04-27 17:39:22 +0000

    - Possibilidade de fazer login com o facebook .

--- trunk/application/views/intranet/login.php
+++ trunk/application/views/intranet/login.php
@@ -1,6 +1,7 @@
 <?php
 
 echo form_open('index.php/intranet/login/cadastrar','id="formNew"');
+echo form_fieldset('Cadastrar');
 echo form_label('Nome:','lbl_nome');
 echo form_input('nome');
 echo form_label('Email:','lbl_email');
@@ -11,7 +12,9 @@ echo form_label('Confirmar senha:','lbl_rpsenha');
 echo form_password('rp_senha');
 echo form_submit('btn_logar','Entrar');
 echo form_close();
+echo form_fieldset_close();
 
+echo form_fieldset('Entrar');
 echo form_open('index.php/intranet/login/entrar','id="formLogin"');
 echo form_label('Email:','lbl_email');
 echo form_input('email');
@@ -19,4 +22,7 @@ echo form_label('Senha:','lbl_senha');
 echo form_password('senha');
 echo form_submit('btn_logar','Entrar');
 echo form_close();
+echo form_fieldset_close();
 ?>
+
+<a href="https://www.facebook.com/dialog/oauth?client_id=<?php echo FB_APP_ID;?>&redirect_uri=<?php echo FB_SITELOGIN;?>&scope=<?php echo FB_SCOPE;?>">Entrar com Facebook</a>
