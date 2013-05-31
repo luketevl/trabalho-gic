83db766ae4a49a988e044b2657703c2ad6eaf01c
luketevl <luketevl@b8303ca9-a0fe-04db-0b07-ae62dc87b2d0>
2013-05-31 22:46:19 +0000

    - teste de merge

--- application/views/intranet/posts_cadastro_list.php
+++ application/views/intranet/posts_cadastro_list.php
@@ -68,10 +68,10 @@
  	<nav id="acoes">
  	<?php 
  			echo form_button('adicionar','<img src='. base_url() .'resources/icons/add.png width= 22 height= 22 /> <span></span>','class="tooltip btn btn-large btn-block btn-primary tooltip" title="Aprovar Posts"');
+ 			echo form_button('editar','<img src='.  base_url() .'resources/icons/edit.png width= 22 height= 22 /> <span></span>','class="tooltip btn btn-large btn-block btn-primary" title="Aprovar Posts"');
  			echo form_button('aprovar','<img src='. base_url() .'resources/icons/aprovar.png width= 22 height= 22 /> <span></span>','class="tooltip btn btn-large btn-block btn-primary tooltip" title="Aprovar Posts"');
  			echo form_button('rejeitar','<img src='. base_url() .'resources/icons/cancel.png width= 22 height= 22 /> <span></span>','class="tooltip btn btn-large btn-block btn-primary" title="Aprovar Posts"');
  			echo form_button('publicar','<img src='. base_url() .'resources/icons/publish.png width= 22 height= 22 /> <span></span>','class="tooltip btn btn-large btn-block btn-primary" title="Aprovar Posts"');
- 			echo form_button('editar','<img src='.  base_url() .'resources/icons/edit.png width= 22 height= 22 /> <span></span>','class="tooltip btn btn-large btn-block btn-primary" title="Aprovar Posts"');
  			echo form_button('remover','<img src='. base_url() .'resources/icons/remove.png width= 22 height= 22 /> <span></span>','class="tooltip btn btn-large btn-block btn-primary" title="Aprovar Posts"');
  	?>
  	</nav>
