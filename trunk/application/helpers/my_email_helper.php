baa09df34b2227e45b8aaee81bdbbf6baeeee87e
luketevl <luketevl@b8303ca9-a0fe-04db-0b07-ae62dc87b2d0>
2013-05-31 22:48:33 +0000

    -helper para e-mail

---
+++ trunk/application/helpers/my_email_helper.php
@@ -0,0 +1,30 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+	
+		if(!function_exists('enviar_email')){
+			function enviar_email($from, $to, $subject,$message,$replay_to="",$mailtype='text'){
+				$config = array(
+					'protocol' => 'smtp',
+						"newline"			=> "\r\n",
+						"smtp_timeout"		=> 30,
+						"validate"			=> TRUE,
+						"smtp_host"			=> 'ssl://smtp.gmail.com',
+						"smtp_port"			=> '465',
+						"smtp_user"			=> 'lukete@gmail.com',
+						"smtp_pass"			=> 'keporra157',
+						"mailtype"			=> $mailtype,
+						);	
+				$this->email->initialize($config);
+				$this->email->from($from);
+				$this->email->to($to);
+				$this->email->cc($cc);
+				$this->email->bcc($cco);
+				$this->email->subject($subject);
+				$this->email->message($message);
+				if (!@$this->email->send()){
+					echo ("Erro no envio de email. [smtp_host: {$config['smtp_host']}] [smtp_port: {$config['smtp_port']}] [smtp_user: {$config['smtp_user']}]");
+					return FALSE;
+				}
+				return TRUE;
+				}
+		}
