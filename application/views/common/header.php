9a4de8f00a917e8a64feab8a9fbb9d8f681a61f2
luketevl <luketevl@b8303ca9-a0fe-04db-0b07-ae62dc87b2d0>
2013-06-18 08:07:15 +0000

    Criando frontend.

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -3,70 +3,87 @@
 <head>
 <meta charset="utf-8">
 <title>Layout Padrao</title>
-<link rel="stylesheet" type="text/css" href="../resources/css/style.css" />
-
+<link rel="stylesheet" href="<?php echo base_url(); ?>resources/css/style.css" />
+	
 <link href="//fonts.googleapis.com/css?family=Montserrat:400,700"
 	rel="stylesheet">
 
-<link href="../resources/icheckmaster/css/demo.css?v=0.7"
-	rel="stylesheet">
+<link rel="stylesheet" href="<?php echo base_url(); ?>resources/icheckmaster/css/demo.css?v=0.7" />
+<link rel="stylesheet" href="<?php echo base_url(); ?>resources/icheckmaster/skins/all.css?v=0.7" />
 
-<link href="../resources/icheckmaster/skins/all.css?v=0.7"
-	rel="stylesheet">
+<script type="text/javascript"
+	src="<?php echo base_url()?>resources/js/common/jquery.js"></script>
+<script type="text/javascript"
+	src="<?php echo base_url()?>resources/sidr-menu-responsive/jquery.sidr.min.js"></script>
+<script type="text/javascript"
+	src="<?php echo base_url(); ?>resources/tooltipster-master/js/jquery.tooltipster.min.js"></script>
+<link rel="stylesheet"
+	href="<?php echo base_url()?>resources/sidr-menu-responsive/stylesheets/jquery.sidr.dark.css" />
+<link rel="stylesheet" type="text/css"
+	href="<?php echo base_url(); ?>resources/tooltipster-master/css/tooltipster.css" />
+
+
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="../resources/icheckmaster/js/jquery.js">\x3C/script>');</script>
 
-<script src="../resources/icheckmaster/js/jquery.icheck.min.js?v=0.7"></script>
-
+<script type="text/javascript"
+	src="<?php echo base_url(); ?>resources/icheckmaster/js/jquery.icheck.min.js?v=0.7"></script>
 
 </head>
 <body>
 	<header id="cabecalho">
 		<nav id="user">
-
-
 			<span>Bem vindo [Apelido] | <a href="#">{[Cadastrar], [Sair]} 
-			
 			</span></a>
-			<?php echo $this->load->view('common/social')?>
 		</nav>
+		<section>
+			<input type="search" autofocus="autofocus" placeholder="Digite sua pesquisa" />
+			<?php 
+			echo form_button('pesquisa','<img  src='. base_url() .'resources/icons/search.png width= 22 height= 22 /> <span></span>','class="tooltip btn btn-large btn-block btn-primary" title="'.lang('btn_apagar').'"');
+			echo form_button('pesquisaGoogle','<img  src='. base_url() .'resources/icons/searchGoogle.png width= 22 height= 22 /> <span></span>','class="tooltip btn btn-large btn-block btn-primary" title="'.lang('btn_apagar').'"');
+				
+			?>
+		</section>
 	</header>
 	<section id="content">
 		<section id="column-left">
+		<iframe src="http://www.google.com" />
+		</iframe>
+
 			<header>Materias</header>
 			<article>
 				<a href="#" title="link">Quaaase! Rachel vai a casa de Livia de
 					surpresa e Wanda se esconde
-					<hr class="clear" /> <img src="../resources/img/teste.jpg"
+					<hr class="clear" /> <img src="<?php echo base_url();?>resources/img/teste.jpg"
 					width="200" alt="Imagem teste" />
 				</a>
 			</article>
 			<article>
 				<a href="#" title="link">Quaaase! Rachel vai a casa de Livia de
 					surpresa e Wanda se esconde
-					<hr class="clear" /> <img src="../resources/img/teste.jpg"
+					<hr class="clear" /> <img src="<?php echo base_url();?>resources/img/teste.jpg"
 					width="200" alt="Imagem teste" />
 				</a>
 			</article>
 			<article>
 				<a href="#" title="link">Quaaase! Rachel vai a casa de Livia de
 					surpresa e Wanda se esconde
-					<hr class="clear" /> <img src="../resources/img/teste.jpg"
+					<hr class="clear" /> <img src="<?php echo base_url();?>resources/img/teste.jpg"
 					width="200" alt="Imagem teste" />
 				</a>
 			</article>
 			<article>
 				<a href="#" title="link">Quaaase! Rachel vai a casa de Livia de
 					surpresa e Wanda se esconde
-					<hr class="clear" /> <img src="../resources/img/teste.jpg"
+					<hr class="clear" /> <img src="<?php echo base_url();?>resources/img/teste.jpg"
 					width="200" alt="Imagem teste" />
 				</a>
 			</article>
 			<article>
 				<a href="#" title="link">Quaaase! Rachel vai a casa de Livia de
 					surpresa e Wanda se esconde
-					<hr class="clear" /> <img src="../resources/img/teste.jpg"
+					<hr class="clear" /> <img src="<?php echo base_url();?>resources/img/teste.jpg"
 					width="200" alt="Imagem teste" />
 				</a>
 			</article>
@@ -74,31 +91,31 @@
 				<a href="#" title="link">Quaaase! Rachel vai a casa de Livia de
 					surpresa e Wanda se esconde
 					<hr class="clear" />
-					<hr class="clear" /> <img src="../resources/img/teste.jpg"
+					<hr class="clear" /> <img src="<?php echo base_url();?>resources/img/teste.jpg"
 					width="200" alt="Imagem teste" />
 				</a>
 			</article>
 			<article>
 				<a href="#" title="link">Quaaase! Rachel vai a casa de Livia de
 					surpresa e Wanda se esconde
-					<hr class="clear" /> <img src="../resources/img/teste.jpg"
+					<hr class="clear" /> <img src="<?php echo base_url();?>resources/img/teste.jpg"
 					width="200" alt="Imagem teste" />
 				</a>
 			</article>
-		</section>
+	</section>
 		<section id="column-right">
 			<header>Videos</header>
 			<article>
 				<a href="#" title="link">Policial e suspenso por foto comprometedora
 					no Facebook e culpa hacker
-					<hr class="clear" /> <img src="../resources/img/teste.jpg"
+					<hr class="clear" /> <img src="<?php echo base_url();?>resources/img/teste.jpg"
 					width="200" alt="Imagem teste" />
 				</a>
 			</article>
 			<article>
 				<a href="#" title="link">Policial e suspenso por foto comprometedora
 					no Facebook e culpa hacker
-					<hr class="clear" /> <img src="../resources/img/teste.jpg"
+					<hr class="clear" /> <img src="<?php echo base_url();?>resources/img/teste.jpg"
 					width="200" alt="Imagem teste" />
 				</a>
 			</article>
@@ -116,3 +133,16 @@
 </html>
 
 
+<script>
+				$(document).ready(function() {
+					  $('.tooltip').tooltipster();
+					$('ul li a').click(function(){
+						var link = $(this).next('input:hidden').val();
+						$('iframe').attr('src',link);
+						$('li').each(function(){
+							$(this).removeAttr('class');
+							});
+						$(this).parent().attr('class',"active");
+						});
+				});
+			</script>
