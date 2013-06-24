41fb3fd3a3fb0d0aa68a22e96be43220efaa1287
luketevl <luketevl@b8303ca9-a0fe-04db-0b07-ae62dc87b2d0>
2013-06-24 18:55:37 +0000

    - Inserido google search

--- trunk/application/views/frontend/home.php
+++ trunk/application/views/frontend/home.php
@@ -79,6 +79,20 @@
 	<?php 
 		$this->load->view('frontend/topo');
 	?>
+	
+	<script>
+  (function() {
+    var cx = '005718714053014607788:oxptsytnkmg';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+        '//www.google.com/cse/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
+<gcse:search></gcse:search>
 		<!-- MAIN -->
 		<div id="main">
 			<div class="wrapper cf">
