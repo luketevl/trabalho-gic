040c1425550ce58541a636f2b65b5c40e5b65d98
luketevl <luketevl@b8303ca9-a0fe-04db-0b07-ae62dc87b2d0>
2013-06-24 17:38:20 +0000

    - Criado HELPER para envio de email. - Após aprovar envia e-mail - Após cadastrar no site envia e-mail.

---
+++ trunk/application/helpers/my_mail_helper.php
@@ -0,0 +1,35 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+require 'class.phpmailer.php';
+
+
+if(!function_exists('enviar_email')){
+	function enviar_email($dest ,$assunto, $conteudo){
+		$mail = new PHPMailer();
+		$mail->IsSMTP();
+		$mail->SMTPDebug  = 0;
+		$mail->Debugoutput = 'html';
+		$mail->Host       = 'smtp.gmail.com';
+		$mail->Port       = 587;
+		$mail->SMTPSecure = 'tls';
+		$mail->SMTPAuth   = true;
+		$mail->Username   = "agropecuariatrabalhogic@gmail.com";
+		$mail->Password   = "Cotemig123456";
+		$mail->SetFrom('noreplay@agropecuariatrabalhogic.com', 'Administracao do Portal');
+		
+		//Adiciona o destinatário
+		$mail->AddAddress($dest);
+		//Adiciona o assunto
+		$mail->Subject = $assunto;
+		//Conteúdo do e-mail
+		$mail->MsgHTML($conteudo);
+		
+		if(@!$mail->Send()){
+// 			  echo "Erro ao enviar email: " . $mail->ErrorInfo;
+		}
+		
+		else{
+// 			echo "E-mail enviado!";
+		}
+	}
+}
