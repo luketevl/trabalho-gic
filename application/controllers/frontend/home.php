1d216e6f9c33360719fea966f889b510cd4ab8e8
luketevl <luketevl@b8303ca9-a0fe-04db-0b07-ae62dc87b2d0>
2013-06-24 06:45:32 +0000

    - Pesquisa

--- application/controllers/frontend/home.php
+++ application/controllers/frontend/home.php
@@ -17,16 +17,24 @@ class Home extends CI_Controller{
 		$dados['dados'] = array();
 		$dados['categorias'] = array();
 		foreach($p->all as $k=>$v){
-		foreach($p->all as $k=>$v){
 			$dados['dados'][$k]['id_post'] = $v->id_post;
 			$dados['dados'][$k]['titulo_post'] = $v->titulo_post;
 			$dados['dados'][$k]['resumo_post'] = $v->resumo_post;
+			$keys = str_replace(' ','_',$v->keywords_post);
+			$keys = strtolower($keys);
+			$keys = explode('|',$keys);
+			foreach($keys as $key=>$valor){
+				$dados['dados'][$k]['chaves'][$key]['chave']= $valor;			
+			}
 			$dados['dados'][$k]['img_principal_post'] = $v->img_principal_post;
 			$dados['dados'][$k]['id_cat'] = $v->id_cat;
 			$c = $c->get_by_id($v->id_cat); 
 			$dados['dados'][$k]['categoria'] = $c->nome_cat;
 			$dados['dados'][$k]['categoria-filter'] =  str_replace(' ','_',$c->nome_cat);
 		}
+// 		echo "<pre>";
+// 		echo print_r($dados);
+// 		echo "</pre>";die;
 		$cTemp = $c->get_vinculados_publicados();
 		foreach($cTemp->all as $k=>$v){
 			$c = $c->get_by_id($v->id_cat);
@@ -57,7 +65,6 @@ class Home extends CI_Controller{
 		}		
 		
 		return $dados;
-		}
 	
 	}
 }
